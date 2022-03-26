1bf4462d66ea381122cf6a507b5dd3654d08360e
rgriao <92741051+rgriao@users.noreply.github.com>
2022-03-26 15:31:36 -0300

    Inicial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>Tentando fazer o HEROKU funfar!/p>, document.getElementById('root'));
+const name = 'Ricardo Grião testando';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
